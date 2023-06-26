b4d6aef4689da4dd64e434b505e1e793de77007b
Evgeny <pet9.wawa.95@gmail.com>
2023-06-26 20:25:03 +0300

    new comm

---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+
+    private String sound;
+
+    public String getSound() {
+        return sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+
+}
